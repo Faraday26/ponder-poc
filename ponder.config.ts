493c0f8eec2ddfa1e1ed283b2410872c011228fc
Faraday <faraday@buffer.finance>
2024-01-10 15:10:57 +0530

    bft tracking graph

--- ponder.config.ts
+++ ponder.config.ts
@@ -15,6 +15,12 @@ export default createConfig({
       abi: BFRAbi,
       address: "0x1a5b0aaf478bf1fda7b934c76e7692d722982a6d",
       network: "arbitrum",
+      filter: {
+        event: "Transfer",
+        args: {
+          to: "0x000000000000000000000000000000000000dEaD",
+        },
+      },
       startBlock: 25921235,
     },
   },
--- src/BFR.ts
+++ src/BFR.ts
@@ -1,9 +1,5 @@
 import { ponder } from "@/generated";
 
-ponder.on("BFR:Approval", async ({ event, context }) => {
-  console.log(event.args);
-});
-
 ponder.on("BFR:Transfer", async ({ event, context }) => {
   console.log(event.args);
 });
